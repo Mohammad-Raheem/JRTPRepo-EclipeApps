4ed7b1cbb28fe184bc5b3b474f1fde0eba6c1848
Mohammad-Raheem <raheemmohammad0410@gmail.com>
2025-03-26 10:13:50 +0530

    "Dev -1 m1() created"

--- MavenProj4Web/src/main/java/com/app/raheem/entity/App.java
+++ MavenProj4Web/src/main/java/com/app/raheem/entity/App.java
@@ -2,7 +2,6 @@ package com.app.raheem.entity;
 
 public class App {
 	public void m1() {
-		System.out.println("======");
-		System.out.println("App.m1()");
+		System.out.println("App.m1() method");
 	}
 }
